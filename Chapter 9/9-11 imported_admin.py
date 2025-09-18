514f37410064227a13d6c32c0646a95a1621ae8c
Isiah S <112279071+irscot@users.noreply.github.com>
2025-09-18 14:51:28 -0500

    Update 9-11 imported_admin.py

--- Chapter 9/9-11 imported_admin.py
+++ Chapter 9/9-11 imported_admin.py
@@ -4,15 +4,16 @@
 from admin_checker import Admin
 
 # Make an admin instance and call describe_user.
-haixis = Admin('haixis', 'sixiah', 'persona 3', 'midnight blue')
-haixis.describe_user()
+isiah = Admin('isiah', 'isaiah', 'persona 3', 'midnight blue')
+isiah.describe_user()
 
 # Call show_privileges.
-haixis_privileges = ['can delete comments',
+isiah_privileges = ['can delete comments',
                    'can mark comments as spoilers',
                    'can ban users']
 
-haixis.privileges.privileges = haixis_privileges
+isiah.privileges.privileges = isiah_privileges
 
-print(f"Admin {haixis.first_name} has the following privileges: ")
-haixis.privileges.show_privileges()
+print(f"Admin {isiah.first_name} has the following privileges: ")
+
+isiah.privileges.show_privileges()
